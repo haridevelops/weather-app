11fb0b2032b6b396fcfce7b041d92b57d1cd4be6
harisudhan <m.harisudhan95@gmail.com>
2019-04-30 23:23:01 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+if (process.argv[2] == null || process.argv[2] == undefined) {
+    console.log('Please input the location to check weather');
+    return;
+}
+
+geocode.getGeoCode(process.argv[2], (err, response) => {
+    if (err) return console.log('error ', err);
+    forecast.getForeCast(response, (err, response) => {
+        if (err) console.log('error ', err);
+        console.log(response);
+    })
+})
